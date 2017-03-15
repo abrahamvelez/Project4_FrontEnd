92ada2a77230e760f7e4c85cbadd5f93b0192850
abrahamvelez <abrahamvelez90@hotmail.gom>
2017-03-14 20:37:20 -0700

    Validation Commit

--- js/main.js
+++ js/main.js
@@ -11,20 +11,23 @@ $(document).ready(function(){
     //Store the input value
     $("#search").keyup(function(){
       var content = $(this).val(), count = 0;
-      console.log(content);
+      // console.log(content);
 
 
     //Loop through the images
     $("img").each(function(){
+      console.log($(this).attr('title'));
         //Fade out results if search does not match pic title
-        if($(this).attr('title').search(new RegExp(content, "i")) < 0){
-           $(this).fadeOut();
-        }
+        if($(this).attr('title')){
+          if ($(this).attr('title').search(new RegExp(content, "i")) < 0){
+              $(this).fadeOut();
+            }else{
+              $(this).show();
+              count++;
+            }
+          }
         //Show results if search does match pic title
-        else{
-          $(this).show();
-          count++;
-        }
+
     });
     // Update the count
     var numberItems = count;
